aa13ba9c3137c4c6436893f1c63fce2d3e4e6c56
Hiba <eng.lilil92@gmail.com>
2022-10-15 19:05:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+function addingEventListener(){
+function clickAlert() {
+    alert("I was clicked");
+    console.log("I was clicked");
 }
+
+input.addEventListener('click', clickAlert);}
+addingEventListener();
+/*
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});*/
